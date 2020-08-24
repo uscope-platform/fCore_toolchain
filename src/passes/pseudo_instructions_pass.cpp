bc19479ff521c8006f56cb97d1a7f1a572d90110
filssavi <filippo.savi@nottingham.edu.cn>
2020-08-24 15:04:11 +0200

    Added license header

--- src/passes/pseudo_instructions_pass.cpp
+++ src/passes/pseudo_instructions_pass.cpp
@@ -1,7 +1,19 @@
-//
-// Created by fils on 06/05/20.
-//
+// Copyright (C) 2020 Filippo Savi - All Rights Reserved
 
+// This file is part of fCore_has.
+
+// fCore_has is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License.
+
+// fCore_has is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with fCore_has.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "pseudo_instructions_pass.hpp"
 
--- test/passes.cpp
+++ test/passes.cpp
@@ -1,6 +1,19 @@
-//
-// Created by fils on 01/05/20.
-//
+// Copyright (C) 2020 Filippo Savi - All Rights Reserved
+
+// This file is part of fCore_has.
+
+// fCore_has is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License.
+
+// fCore_has is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with fCore_has.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "includes/catch.hpp"
 #include "../src/code_elements/for_loop.hpp"
@@ -9,10 +22,6 @@
 #include "../src/passes/passes.hpp"
 #include "../src/backend/output_generator.hpp"
 #include "../src/frontend/file_parser.h"
-
-
-
-
 
 
 TEST_CASE( "pseudo_inst_pass") {
