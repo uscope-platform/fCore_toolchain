b77b712c5b75e814b011e979fe49a5100a86c36b
Filippo Savi <filisavi@unimore.it>
2021-07-09 12:00:16 +0200

    renamed a bunch more stuff

---
+++ includes/ast/transformations/high_level_ast_lowering.h
@@ -0,0 +1,14 @@
+//
+// Created by fils on 09/07/2021.
+//
+
+#ifndef FCORE_HAS_HIGH_LEVEL_AST_LOWERING_H
+#define FCORE_HAS_HIGH_LEVEL_AST_LOWERING_H
+
+
+class high_level_ast_lowering {
+
+};
+
+
+#endif //FCORE_HAS_HIGH_LEVEL_AST_LOWERING_H
---
+++ src/ast/transformations/high_level_ast_lowering.cpp
@@ -0,0 +1,5 @@
+//
+// Created by fils on 09/07/2021.
+//
+
+#include "ast/transformations/high_level_ast_lowering.h"
