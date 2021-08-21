86474d19cfef85ef1cde41f178cdbac91390bdd0
Filippo Savi <filisavi@unimore.it>
2021-08-21 10:18:48 +0200

    Added proper license header to  a bunhc of places

--- src/passes/high_level/intrinsics_implementation_pass.cpp
+++ src/passes/high_level/intrinsics_implementation_pass.cpp
@@ -1,6 +1,19 @@
-//
-// Created by fils on 12/07/2021.
-//
+// Copyright (C) 2020 Filippo Savi - All Rights Reserved
+
+// This file is part of fCore_toolchain.
+
+// fCore_toolchain is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License.
+
+// fCore_toolchain is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with fCore_toolchain.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "passes/high_level/intrinsics_implementation_pass.hpp"
 
