fc84296616147f0086911b7de26f3f5a272b8705
filippo savi <filippo.savi@unimore.it>
2024-04-30 09:27:00 +0200

    Fix file path issue

--- includes/emulator/backend/emulator.hpp
+++ includes/emulator/backend/emulator.hpp
@@ -24,7 +24,7 @@
 #include "data_structures/emulation/emulator_metadata.hpp"
 
 #if GENERAL_PURPOSE_EMULATION==1
-    #include "emulator/gp_executor.hpp"
+    #include "emulator/backend/gp_executor.hpp"
 #else
     #include "emulator/backend/ba_executor.hpp"
 #endif
