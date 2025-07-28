5088623eb160e7803956ee81d58392d32d06be68
filippo savi <filssavi@gmail.com>
2025-07-28 10:07:34 +0200

    added few more tests for multirate emulation

--- test/emulator/full_testing/emulator_multirate.cpp
+++ test/emulator/full_testing/emulator_multirate.cpp
@@ -131,3 +131,288 @@ TEST(emulator_multirate, emulator_simple_multirate) {
     }
 
 }
+
+
+TEST(emulator_multirate, emulator_input_multirate) {
+
+    nlohmann::json specs = nlohmann::json::parse( R"(
+        {
+            "version": 2,
+            "cores": [
+                {
+                    "id": "test_0",
+                    "order": 1,
+                    "inputs": [
+                        {
+                            "name": "in",
+                            "is_vector": false,
+                            "metadata":{
+                                "type": "float",
+                                "width": 12,
+                                "signed":true,
+                                "common_io": false
+                            },
+                            "source": {
+                                "type": "series",
+                                "value": [1, 1000, 2, 2000]
+                            }
+                        }
+                    ],
+                    "outputs": [],
+                    "memory_init": [
+                        {
+                            "name": "mem",
+                            "is_vector": false,
+                            "metadata": {
+                                "type": "float",
+                                "width": 32,
+                                "signed": false
+                            },
+                            "is_output": true,
+                            "is_input": false,
+                            "value": 0
+                        }
+                    ],
+                    "channels": 1,
+                    "options": {
+                        "comparators": "reducing",
+                        "efi_implementation": "efi_sort"
+                    },
+                    "program": {
+                        "content": "int main(){float mem; mem += in;}",
+                        "type": "c",
+                        "headers": []
+                    },
+                    "sampling_frequency": 1,
+                    "deployment": {
+                        "has_reciprocal": false,
+                        "control_address": 18316525568,
+                        "rom_address": 17179869184
+                    }
+                },
+                {
+                    "id": "test_1",
+                    "order": 2,
+                    "inputs": [
+                        {
+                            "name": "in",
+                            "is_vector": false,
+                            "metadata":{
+                                "type": "float",
+                                "width": 12,
+                                "signed":true,
+                                "common_io": false
+                            },
+                            "source": {
+                                "type": "series",
+                                "value": [1, 1, 2, 2]
+                            }
+                        }
+                    ],
+                    "outputs": [],
+                    "memory_init": [
+                        {
+                            "name": "mem",
+                            "is_vector": false,
+                            "metadata": {
+                                "type": "float",
+                                "width": 32,
+                                "signed": false
+                            },
+                            "is_output": true,
+                            "is_input": false,
+                            "value": 0
+                        }
+                    ],
+                    "channels": 1,
+                    "options": {
+                        "comparators": "reducing",
+                        "efi_implementation": "efi_sort"
+                    },
+                    "program": {
+                        "content": "int main(){float mem; mem += in;}",
+                        "type": "c",
+                        "headers": []
+                    },
+                    "sampling_frequency": 2,
+                    "deployment": {
+                        "has_reciprocal": false,
+                        "control_address": 18316525568,
+                        "rom_address": 17179869184
+                    }
+                }
+            ],
+            "interconnect": [],
+            "emulation_time": 2,
+            "deployment_mode": false
+        }
+    )");
+
+
+    emulator_dispatcher manager;
+    manager.set_specs(specs);
+    manager.process();
+    manager.emulate();
+
+    auto res_obj = manager.get_results();
+
+    auto dbg = res_obj.dump(4);
+
+    std::vector<float> res_1 = res_obj["test_0"]["outputs"]["mem"]["0"][0];
+    std::vector<float> reference_1 = {1.0, 1.0, 3.0, 3.0};
+    for (size_t i = 0; i < res_1.size(); i++) {
+        EXPECT_EQ(res_1[i], reference_1[i]);
+    }
+
+    std::vector<float> res_2= res_obj["test_1"]["outputs"]["mem"]["0"][0];
+    std::vector<float> reference_2 = {1.0, 2.0, 4.0, 6.0};
+    for (size_t i = 0; i < res_2.size(); i++) {
+        EXPECT_EQ(res_2[i], reference_2[i]);
+    }
+
+}
+
+
+TEST(emulator_multirate, emulator_interconnect_multirate) {
+
+    nlohmann::json specs = nlohmann::json::parse( R"(
+        {
+            "version": 2,
+            "cores": [
+                {
+                    "id": "test_0",
+                    "order": 1,
+                    "inputs": [
+                        {
+                            "name": "in",
+                            "is_vector": false,
+                            "metadata":{
+                                "type": "float",
+                                "width": 12,
+                                "signed":true,
+                                "common_io": false
+                            },
+                            "source": {
+                                "type": "series",
+                                "value": [1, 1, 5, 5]
+                            }
+                        }
+                    ],
+                    "outputs": [],
+                    "memory_init": [
+                        {
+                            "name": "mem",
+                            "is_vector": false,
+                            "metadata": {
+                                "type": "float",
+                                "width": 32,
+                                "signed": false
+                            },
+                            "is_output": true,
+                            "is_input": false,
+                            "value": 0
+                        }
+                    ],
+                    "channels": 1,
+                    "options": {
+                        "comparators": "reducing",
+                        "efi_implementation": "efi_sort"
+                    },
+                    "program": {
+                        "content": "int main(){float mem; mem += in;}",
+                        "type": "c",
+                        "headers": []
+                    },
+                    "sampling_frequency": 1,
+                    "deployment": {
+                        "has_reciprocal": false,
+                        "control_address": 18316525568,
+                        "rom_address": 17179869184
+                    }
+                },
+                {
+                    "id": "test_1",
+                    "order": 2,
+                    "inputs": [
+                        {
+                            "name": "in",
+                            "is_vector": false,
+                            "metadata":{
+                                "type": "float",
+                                "width": 12,
+                                "signed":true,
+                                "common_io": false
+                            },
+                            "source": {
+                                "type": "series",
+                                "value": [1, 1, 2, 2]
+                            }
+                        }
+                    ],
+                    "outputs": [],
+                    "memory_init": [
+                        {
+                            "name": "mem",
+                            "is_vector": false,
+                            "metadata": {
+                                "type": "float",
+                                "width": 32,
+                                "signed": false
+                            },
+                            "is_output": true,
+                            "is_input": false,
+                            "value": 0
+                        }
+                    ],
+                    "channels": 1,
+                    "options": {
+                        "comparators": "reducing",
+                        "efi_implementation": "efi_sort"
+                    },
+                    "program": {
+                        "content": "int main(){float mem; mem += in;}",
+                        "type": "c",
+                        "headers": []
+                    },
+                    "sampling_frequency": 2,
+                    "deployment": {
+                        "has_reciprocal": false,
+                        "control_address": 18316525568,
+                        "rom_address": 17179869184
+                    }
+                }
+            ],
+            "interconnect": [
+                {
+                    "source": "test_0.mem",
+                    "destination":"test_1.in"
+                }
+            ],
+            "emulation_time": 2,
+            "deployment_mode": false
+        }
+    )");
+
+
+    emulator_dispatcher manager;
+    manager.set_specs(specs);
+    manager.process();
+    manager.emulate();
+
+    auto res_obj = manager.get_results();
+
+    auto dbg = res_obj.dump(4);
+
+    std::vector<float> res_1 = res_obj["test_0"]["outputs"]["mem"]["0"][0];
+    std::vector<float> reference_1 = {1.0, 1.0, 6.0, 6.0};
+    for (size_t i = 0; i < res_1.size(); i++) {
+        EXPECT_EQ(res_1[i], reference_1[i]);
+    }
+
+    std::vector<float> res_2= res_obj["test_1"]["outputs"]["mem"]["0"][0];
+    std::vector<float> reference_2 = {1.0, 2.0, 8.0, 14.0};
+    for (size_t i = 0; i < res_2.size(); i++) {
+        EXPECT_EQ(res_2[i], reference_2[i]);
+    }
+
+}
